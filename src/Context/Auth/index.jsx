7ec3cde3929b380f91b321a8c453be7d4e352803
SamaadTurner <samaad.turner@gmail.com>
2023-11-02 23:04:48 -0700

    lab 34 almost done

--- src/Context/Auth/index.jsx
+++ src/Context/Auth/index.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
+const URL = import.meta.env.VITE_BASE_URL;
 // import testUsers from './lib/testUsers';
 //import {jwtDecode } from "jwt-decode";
 import cookie from 'react-cookies';
-//import axios from 'axios';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
 
 export const AuthContext = React.createContext();
 
@@ -11,15 +13,19 @@ function AuthProvider({children}){
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState({});
     const [error, setError] = useState(null);
+    const [token, setToken] = useState(null);
 
     useEffect(() => {
         let cookieToken = cookie.load('auth')
-        _validateToken(cookieToken);
+        if(cookieToken){
+            _validateToken(cookieToken);
+        }
     }, []);
 
     const _validateToken = (token) => {
         try {
-            let validUser = {username: token, capabilities: ['read','create', 'update', 'delete']};
+            let validUser = jwtDecode(token);
+            console.log(validUser);
             
             if(validUser){
                 cookie.save('auth', token)
@@ -27,6 +33,7 @@ function AuthProvider({children}){
                 setIsLoggedIn(true);
             }
             setIsLoggedIn(true);
+            setToken(token);
         } catch(e){
             setError(e);
             console.log(e);
@@ -34,16 +41,17 @@ function AuthProvider({children}){
     }
 
     const login = async (username, password) => {
-        // let config = {
-        //     baseURL: 'https://api-js401.herokuapp.com',
-        //     url: '/signin',
-        //     method: 'post',
-        //     auth: { username, password },
-        // };
-        // let response = await axios(config);
-        let token = username;
+        let config = {
+            baseURL: URL,
+            url: '/signin',
+            method: 'post',
+            auth: { username, password },
+        };
+        let response = await axios(config);
+        console.log(response);
+        let token = response.data.token;
         
-        if(username && password){
+        if(token){
             try{
                 _validateToken(token);
             } catch(e){
@@ -67,9 +75,11 @@ function AuthProvider({children}){
         isLoggedIn,
         user,
         error,
+        token,
         login,
         logout,
         can,
+        
     }
 
     return(
